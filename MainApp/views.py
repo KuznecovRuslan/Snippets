aadec26e52db0f62caf6bf89e658145a640aaf3e
ruslan <kuznecov.rusaln@gmail.com>
2021-10-11 22:30:19 +0300

    Форма редактирования просто создаёт новые записи

--- MainApp/views.py
+++ MainApp/views.py
@@ -1,5 +1,7 @@
-from django.http import Http404
-from django.shortcuts import render, redirect
+from django.http import Http404, HttpResponse
+from django.shortcuts import render, redirect, get_object_or_404
+from MainApp.models import Snippet
+from MainApp.forms import SnippetForm
 
 
 def index_page(request):
@@ -8,10 +10,61 @@ def index_page(request):
 
 
 def add_snippet_page(request):
-    context = {'pagename': 'Добавление нового сниппета'}
-    return render(request, 'pages/add_snippet.html', context)
+    if request.method == "GET":
+        form = SnippetForm()
+        context = {'pagename': 'Добавление нового сниппета'}
+        context["form"] = form
+        return render(request, 'pages/add_snippet.html', context)
+    elif request.method == "POST":
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
 
 
 def snippets_page(request):
     context = {'pagename': 'Просмотр сниппетов'}
+
+    snippets = Snippet.objects.all
+
+    context = {
+        "snippets": snippets,
+
+    }
+
     return render(request, 'pages/view_snippets.html', context)
+
+
+def snippets_create(request):
+
+    if request.method == "POST":
+        # print(request.POST)
+
+        # name = request.POST["name"]
+        # lang = request.POST["lang"]
+        # code = request.POST["code"]
+        # snippet = Snippet(name=name, lang=lang, code=code)
+        # snippet.save()
+        form = SnippetForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
+
+    raise Http404
+
+
+def snippet_edit(request, id):
+    if request.method == "GET":
+        snippet = get_object_or_404(Snippet, pk=id)
+        form = SnippetForm(instance=snippet)
+
+        context = {'pagename': 'Изменение сниппета ' + str(id)}
+        context["form"] = form
+        return render(request, 'pages/add_snippet.html', context)
+
+    if request.method == "POST":
+        snippet = get_object_or_404(Snippet, pk=id)
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.is_valid():
+            form.save()
+            return redirect('snippets-list')
--- Snippets/urls.py
+++ Snippets/urls.py
@@ -22,5 +22,7 @@ urlpatterns = [
     path('', views.index_page, name='home'),
     path('snippets/add', views.add_snippet_page, name='snippets-add'),
     path('snippets/list', views.snippets_page, name='snippets-list'),
+    path('snippets/create', views.snippets_create, name='snippets-create'),
+    path('snippet/edit/<int:id>', views.snippet_edit, name='snippet-edit'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
